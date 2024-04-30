f49c9cda22a2ba49d4b21ec0fd144558d57fc3f3
khalidbinibrahim <khalidbinibrahim01@gmail.com>
2024-04-30 21:53:30 +0600

    added environment variables

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCIcQKHTJYmv6Tnd_VtZnhNdN2imQJQteE",
-  authDomain: "khalid-bin-ibrahim-a10.firebaseapp.com",
-  projectId: "khalid-bin-ibrahim-a10",
-  storageBucket: "khalid-bin-ibrahim-a10.appspot.com",
-  messagingSenderId: "924871530570",
-  appId: "1:924871530570:web:fee5fa8e9cd10a3f54aeb3"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
